8cba388371c712fa652564b564aa0f59cabd5a2d
Martin Wernstål <m4rw3r@gmail.com>
2014-02-22 01:13:10 +0100

    ACL: Initial tests

---
+++ acl_test.go
@@ -0,0 +1,91 @@
+package acl
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"lab.likipe.se/worktaim-api/config"
+	"lab.likipe.se/worktaim-api/postgresql"
+	"lab.likipe.se/worktaim-api/util"
+)
+
+type idAble struct {
+	id string
+}
+
+func (r idAble) GetId() string {
+	return r.id
+}
+
+func TestAcl(t *testing.T) {
+	config := config.LoadConfiguration("../config_test.json")
+
+	db := postgresql.CreateConnection(config)
+
+	db.Execf(`DROP TABLE IF EXISTS "ACL_Test";
+
+CREATE TABLE "ACL_Test"
+(
+	"aro_id" uuid NOT NULL,
+	"action" character varying(255) NOT NULL,
+	"resource_id" uuid,
+	PRIMARY KEY ("aro_id", "action", "resource_id")
+);`)
+
+	testUserAllowed   := idAble{id: "3eb9e0dc-72fa-4e8f-a188-dcca409220f9"}
+	testUserForbidden := idAble{id: "4a567886-2de1-4b0b-9508-5e3125da30f8"}
+
+	testResourceA := idAble{id: "e74dc49c-e663-4144-9383-1a09c6c7ddfd"}
+	// testResourceB := idAble{id: "98a66485-7c49-4f71-ad3c-6db457d54335"}
+
+	acl := NewACL(db, "ACL_Test")
+
+	Convey("With an empty database", t, func() {
+		Convey("It should always deny access requests without a bypassFunc", func() {
+			allowed, err := acl.AllowsAction(testUserAllowed, "test")
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, false)
+
+			allowed, err = acl.AllowsActionOn(testUserAllowed, "test", testResourceA)
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, false)
+
+			allowed, err = acl.AllowsAction(testUserForbidden, "test")
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, false)
+
+			allowed, err = acl.AllowsActionOn(testUserForbidden, "test", testResourceA)
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, false)
+		})
+
+		Convey("It should always deny access requests when the bypassFunc false", func() {
+			aclWithBypass := NewACLWithBypass(db, "ACL_Test", func(actor Resource, action string, target Resource) bool {
+				return false
+			})
+
+			allowed, err := aclWithBypass.AllowsAction(testUserAllowed, "test")
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, false)
+
+			allowed, err = aclWithBypass.AllowsActionOn(testUserAllowed, "test", testResourceA)
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, false)
+		})
+
+		Convey("It should always allow access requests when the bypassFunc returns true", func() {
+			aclWithBypass := NewACLWithBypass(db, "ACL_Test", func(actor Resource, action string, target Resource) bool {
+				return true
+			})
+
+			allowed, err := aclWithBypass.AllowsAction(testUserAllowed, "test")
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, true)
+
+			allowed, err = aclWithBypass.AllowsActionOn(testUserAllowed, "test", testResourceA)
+			util.PanicIf(err)
+			So(allowed, ShouldEqual, true)
+		})
+	})
+}
